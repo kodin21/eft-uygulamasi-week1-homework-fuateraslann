dda0bea6acc1c3ae23e23818b3e82a0896fdfe68
fuat <eraslan.fuat@metu.edu.tr>
2021-07-09 22:46:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 import { name } from './example-other-file';
+const timeOut = 60 * 1000 * 2;
 
-console.log('Javascriptinizi buraya yazabilirsiniz')
+/*window.addEventListener("load", ()=>{
+    window.setTimeout(() => {
+        alert("Oturumunuz sonlanmıştır");
+        window.location.reload(true);
+    }, timeOut);
 
+});*/
+
+let hesapSecildi =0 , iban_girildi =0 ,miktarUygun =0;
+let hesapOption;
+var hesapCntrl = document.getElementById("dropdownMenu");
+hesapCntrl.addEventListener("change", function() {
+    if(hesapCntrl.value!==0)
+        hesapSecildi =1;
+    else
+        hesapSecildi=0;
+    hesapOption = hesapOption = hesapCntrl.getElementsByTagName("option")[hesapCntrl.selectedIndex];
+    butonAktif();
+});
+let ibanCntrl = document.getElementById("iban")
+ibanCntrl.addEventListener("input",function (){
+    if(ibanCntrl.value.length>11)
+        ibanCntrl.value = ibanCntrl.value.slice(0,11);
+})
+ibanCntrl.addEventListener("change",function (){
+    if(ibanCntrl.value.length<11){
+        iban_girildi=0;
+        alert("iban 11 hane olmalıdır")
+    }
+    else iban_girildi =1;
+    butonAktif();
+})
+let paraCntrl = document.getElementById("para");
+paraCntrl.addEventListener("change",function (){
+    console.log(paraCntrl.value)
+    console.log(hesapOption.value)
+    if(paraCntrl.value < parseInt(hesapOption.value) )
+        miktarUygun=1;
+    else {
+        miktarUygun =0 ;
+        alert("Hesap bakiyesi yetersizdir!")
+    }
+    butonAktif();
+})
+function butonAktif(){
+    if(hesapSecildi && iban_girildi && miktarUygun)
+        document.getElementById("gonder").disabled = false;
+    else document.getElementById("gonder").disabled = true;
+}
 // Baska dosyadan calistirmak mumkun, dosyalari parcalamaktan korkmayin!
 // name
